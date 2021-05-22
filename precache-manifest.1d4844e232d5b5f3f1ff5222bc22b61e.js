921ff329dc22e56ae67557c96f9a35d60abd16cc
gfyre <drumpatel@gmail.com>
2021-05-22 18:48:26 +0530

    Updates

---
+++ precache-manifest.1d4844e232d5b5f3f1ff5222bc22b61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb710c68ec9512ac34f11289b356640",
+    "url": "/trading-platform/index.html"
+  },
+  {
+    "revision": "69c0b6a960e3cc63f20d",
+    "url": "/trading-platform/static/css/2.890262d6.chunk.css"
+  },
+  {
+    "revision": "7473b99e2f9e28ea118d",
+    "url": "/trading-platform/static/css/main.dfe6f74c.chunk.css"
+  },
+  {
+    "revision": "69c0b6a960e3cc63f20d",
+    "url": "/trading-platform/static/js/2.af657766.chunk.js"
+  },
+  {
+    "revision": "7473b99e2f9e28ea118d",
+    "url": "/trading-platform/static/js/main.da6e5cbf.chunk.js"
+  },
+  {
+    "revision": "9766f7e32917672bf06e",
+    "url": "/trading-platform/static/js/runtime~main.3052ded6.js"
+  }
+]);
